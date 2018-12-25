ea41e5dfabe1b1500759fe4e1da15a9d14c4305d
ropelife <anjali9jan1999@gmail.com>
2018-12-26 00:16:13 +0530

    partA done!

---
+++ partA.php
@@ -0,0 +1,458 @@
+<?php
+
+include 'top.php';
+
+?>
+
+	<nav class="navbar bg-dark">
+		<p class="navbar-brand" style="color:white;width:100%;text-align:center"></p>
+	</nav>
+    
+   
+  
+    <div class="container parta">
+    	<header class="heading"><b>Part A: GENERAL INFORMATION</b></header>
+    	<hr style="border: 0.5px solid grey"><br>
+    	<div class="row">
+    		<div class="col-md-12">
+    			<div class="row">
+    				<div class="col-md-6">
+		    			<div class="form-group row">
+		    				<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Name</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							   <input class="form-control partalabel" type="text" name="faculty_name" id="example-text-input" required />
+							</div>
+						</div>
+
+							
+		    		</div>
+		    		<div class="col-md-6">
+		    			<div class="form-group row">
+							<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Emp. Code</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							    <input class="form-control partalabel" type="text" name="ecode" id="example-text-input" required />
+							</div>
+						</div>
+		    		</div>
+    			</div><br>
+    			<div class="row">
+    				<div class="col-md-6">
+		    			<div class="form-group row">
+		    				<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Present Address</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							   <input class="form-control partalabel" type="text" name="praddr" id="example-text-input" required />
+							</div>
+						</div>
+
+							
+		    		</div>
+		    		<div class="col-md-6">
+		    			<div class="form-group row">
+		    				<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Permanent Address</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							   <input class="form-control partalabel" type="text" name="peaddr" id="example-text-input" required />
+							</div>
+						</div>
+		    		</div>
+    			</div>
+    			<div class="row">
+    				<div class="col-md-6">
+		    			<div class="form-group row">
+		    				<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Email</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							   <input class="form-control partalabel" type="email" name="email" id="example-text-input" required />
+							</div>
+						</div>					
+		    		</div>
+		    		<div class="col-md-6">
+		    			<div class="form-group row">
+							<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Mobile No.</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							   <input class="form-control partalabel" type="text" name="mobileno" id="example-text-input" required />
+							</div>	  
+						</div>
+		    		</div>
+    			</div><br>
+    			<div class="row">
+    				<div class="col-md-6">
+		    			<div class="form-group row">
+		    				<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Highest qualification</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							   <input class="form-control partalabel" type="text" name="highq" id="example-text-input" required />
+							</div>
+						</div>
+
+							
+		    		</div>
+		    		<div class="col-md-6">
+		    			<div class="form-group row">
+							<div class="col-3">
+		    					<label for="example-text-input" class="col-form-label">Date of Birth</label>
+		    				</div>
+							  
+							<div class="col-8" style="padding-left: 0">
+							   <input class="form-control partalabel" type="date" name="dob" id="example-text-input" required />
+							</div>
+						</div>
+		    		</div>
+    			</div>
+    		</div>
+    	</div>		
+    	<hr style="border: 1px solid grey">
+
+    	<div class="row">
+    		<div class="col-md-6 text-left">
+    			<p style="font-size: 18px"><b>Details of last service i.e before joining KJSCE:-</b></p>
+    		</div>
+    	</div><br>
+
+    	<div class="row">
+    		<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Designation</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="desi" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Name of Organization</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="nameo" id="example-text-input" required />
+					</div>
+				</div>
+			</div>		
+		</div>
+		<hr style="border: 1px solid grey"><br>
+
+		<div class="row">
+    		<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Present Designation</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="pdesi" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">DOJ KJSCE</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="dojkjsce" id="example-text-input" required />
+					</div>
+				</div>
+			</div>		
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Present Scale</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="number" name="pscale" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Present basic and grade pay</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="number" name="pbg" id="example-text-input" required />
+					</div>
+				</div>
+			</div>		
+		</div>
+		<hr style="border: 1px solid grey">
+
+		<div class="row">
+    		<div class="col-md-5 text-left">
+    			<p style="font-size: 18px"><b>Details of last promotion by selection:-</b></p>
+    		</div>
+    	</div><br>
+    	<div class="row">
+    		<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Designation</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="lastdesisel" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Promotion w.e.f</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="promowef" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Changed Scale</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="number" name="cscales" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Changed basic and grade pay</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="number" name="cbasics" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr style="border: 1px solid grey">
+
+		<div class="row">
+    		<div class="col-md-5 text-left">
+    			<p style="font-size: 18px"><b>Details of last promotion through CAS:-</b></p>
+    		</div>
+    	</div>
+
+    	<div class="row">
+    		<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Designation</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="lastdesicas" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Promotion w.e.f</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="promowefcas" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Changed Scale</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control" type="number" name="cscalecas" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Changed basic and grade pay</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control" type="number" name="cbasiccas" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr style="border: 1px solid grey">
+
+		<div class="row">
+			<div class="col-md-6">
+				<p>Whether acquired any fresh educational qualification during said academic year?</p>
+			</div>
+	    	<div class="col-md-6 text-left">
+				<div class="custom-control custom-radio custom-control-inline">
+				  <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input yesradio">
+				  <label class="custom-control-label yes" for="customRadioInline1">Yes</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+				  <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input noradio">
+				  <label class="custom-control-label no" for="customRadioInline2">No</label>
+				</div>	
+				<div class="custom-control custom-radio custom-control-inline">
+				<p>If yes: 20 PI</p>
+			</div>
+			</div>
+		</div><br>
+
+		<div class="row">
+			<div class="col text-left">
+				<p>If yes, details of qualification:-</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class=" col-form-label">Name of Degree</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="nameofdegree" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+	    		<div class="form-group row">
+					<div class="col-3">
+	    				<label for="example-text-input" class="col-form-label">Institute</label>
+	    			</div>
+						  
+					<div class="col-8" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="institute" id="example-text-input" required />
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr style="border: 0.5px solid grey">
+
+	    		<div class="form-group row">
+					<div class="col-md-6 text-left">
+	    				<label for="example-text-input" class="col-form-label">20 PI for Ph.D and M.Phil. 10 PI for other UG/PG Degree/Diploma/Certificate Courses/</label>
+	    			</div>
+						  
+					<div class="col-md-5" style="padding-left: 0">
+						<input class="form-control partalabel" type="text" name="institute" id="example-text-input" required />
+					</div>
+				</div>
+		<hr style="border: 0.5px solid grey">
+
+		<div class="row">
+    		<div class="col-md-12">
+    			<p style="font-size: 18px"><b>Details of course/summer school/STTP/online course attended/completed during academic year</b></p>
+    		</div>
+    	</div>
+
+		<div class="panel panel-default">
+		  
+		  	<div class="panel-body">
+		  
+		  		<div id="education_fields"></div>
+
+		  		<div class="row form-inline justify-content-center">
+		  			<div class="nopadding">
+				  		<div class="form-group">
+				    		<input type="text" class="dynamic-four" id="srno" name="srno[]" value="" placeholder="Sr.no">
+				  		</div>
+					</div>
+
+					<div class="nopadding">
+				  		<div class="form-group">
+				    		<input type="text" class="dynamic-four" id="course" name="course[]" value="" placeholder="Name of summer school/course">
+				  		</div>
+					</div>
+
+					<div class="nopadding">
+				  		<div class="form-group">
+				   			<input type="text" class="dynamic-four" id="days" name="days[]" value="" placeholder="Duration(days)">
+				  		</div>
+					</div>
+
+					<div class="nopadding">
+						<div class="form-group">
+				    		<input type="text" class="dynamic-four" id="agency" name="agency[]" value="" placeholder="Organising Agency">
+				  		</div>
+					</div>
+				  	<div class="input-group-btn">
+				        <img src="https://img.icons8.com/color/48/000000/plus.png" onclick="education_fields();" style="cursor:pointer" />
+				    </div>
+
+				<div class="clear"></div>		
+			</div>
+		 
+	
+		</div>
+
+
+
+
+
+	</div>
+
+	<script type="text/javascript">
+
+	var room = 1;
+	function education_fields() {
+	 
+	    room++;
+	    var objTo = document.getElementById('education_fields')
+	    var divtest = document.createElement("div");
+		divtest.setAttribute("class", "form-group removeclass"+room);
+		var rdiv = 'removeclass'+room;
+	    divtest.innerHTML = '<div class="row form-inline justify-content-center"><div class="nopadding"><div class="form-group"><input type="text" class="dynamic-four" id="srno" name="srno[]" value="" placeholder="Sr.no"></div></div><div class="nopadding"><div class="form-group"><input type="text" class="dynamic-four" id="course" name="course[]" value="" placeholder="Name of summer school/course"></div></div><div class="nopadding"><div class="form-group"><input type="text" class="dynamic-four" id="days" name="days[]" value="" placeholder="Duration(days)"></div></div><div class="nopadding"><div class="form-group"><input type="text" class="dynamic-four" id="agency" name="agency[]" value="" placeholder="Organising Agency"></div></div><div class="input-group-btn"> <img src="https://img.icons8.com/color/48/000000/minus.png" onclick="remove_education_fields('+ room +');" style="cursor:pointer"> </div></div><div class="clear"></div></div>';
+	    
+	    objTo.appendChild(divtest);
+	}
+	function remove_education_fields(rid) {
+		$('.removeclass'+rid).remove();
+	}
+
+	</script>
+
+
+
+		    
--- top.php
+++ top.php
@@ -6,6 +6,7 @@
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	
 	<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
